00a9db57d04891672e4890526047b91a3be09cb7
Atul Behera <v-atulbehera@microsoft.com>
2020-11-27 11:49:26 +0530

    Updated Event formatting

---
+++ src/constant.ts
@@ -0,0 +1,242 @@
+module powerbi.extensibility.visual {
+    export const monthNum: any = {
+        January: '1',
+        February: '2',
+        March: '3',
+        April: '4',
+        May: '5',
+        June: '6',
+        July: '7',
+        August: '8',
+        September: '9',
+        October: '10',
+        November: '11',
+        December: '12'
+    };
+    export const monthName: any = {
+        1: 'january',
+        2: 'february',
+        3: 'march',
+        4: 'april',
+        5: 'may',
+        6: 'june',
+        7: 'july',
+        8: 'august',
+        9: 'september',
+        10: 'october',
+        11: 'november',
+        12: 'december'
+    };
+     
+    export const monthNumGerman: any = {
+        Januar: '1',
+        Februar: '2',
+        März: '3',
+        April: '4',
+        Mai: '5',
+        Juni: '6',
+        Juli: '7',
+        August: '8',
+        September: '9',
+        Oktober: '10',
+        November: '11',
+        Dezember: '12'
+    };
+    export const monthNameGerman: any = {
+        1: 'januar',
+        2: 'februar',
+        3: 'märz',
+        4: 'april',
+        5: 'mai',
+        6: "juni",
+        7: 'juli',
+        8: 'august',
+        9: 'september',
+        10: 'oktober',
+        11: "november",
+        12: 'dezember'
+    };
+    export const shortmonthNumGerman: any = {
+        Jan: '1',
+        Feb: '2',
+        März: '3',
+        Apr: '4',
+        Mai: '5',
+        Juni: '6',
+        Juli: '7',
+        Aug: '8',
+        Sep: '9',
+        Okt: '10',
+        Nov: '11',
+        Dez: '12'
+    };
+   
+    export const monthNumFrench: any = {
+        janvier: '1',
+        février: '2',
+        mars: '3',
+        avril: '4',
+        mai: '5',
+        juin: '6',
+        juillet: '7',
+        août: '8',
+        septembre: '9',
+        octobre: '10',
+        novembre: '11',
+        décembre: '12'
+    };
+    export const monthNameFrench: any = {
+        1: 'janvier',
+        2: 'février',
+        3: 'mars',
+        4: 'avril',
+        5: 'mai',
+        6: 'juin',
+        7: 'juillet',
+        8: 'août',
+        9: 'septembre',
+        10: 'octobre',
+        11: 'novembre',
+        12: 'décembre'  
+    };
+    export const shortmonthNumFrench: any = {
+        janv: '1',
+        févr: '2',
+        mar: '3',
+        avril: '4',
+        mai: '5',
+        juin: '6',
+        jul: '7',
+        août: '8',
+        sept: '9',
+        oct: '10',
+        nov: '11',
+        déc: '12'
+    };
+    export const monthNumGalician: any = {
+         xaneiro: "1",
+         febreiro: "2",
+         marzo: "3",
+         abril: "4",
+         maio: "5",
+         xuño: "6",
+         xullo: "7",
+         agosto: "8",
+         setembro: "9",
+         outubro: "10",
+         novembro: "11",
+         decembro: "12"
+    };
+    export const monthNameGalician: any = {
+        1: "xaneiro",
+         2: "febreiro",
+         3: "marzo",
+         4: "abril",
+         5: "maio",
+         6: "xuño",
+         7: "xullo",
+         8: "agosto",
+         9: "setembro",
+         10: "outubro",
+         11: "novembro",
+         12: "decembro"
+    };
+    export const shortmonthNumGalician: any = {
+        xan: "1",
+        feb: "2",
+        mar: "3",
+        abr: "4",
+        mai: "5",
+        xuñ: "6",
+        xul: "7",
+        ago: "8",
+        set: "9",
+        out: "10",
+        nov: "11",
+        dec: "12"
+    };
+    export const monthNumPortuguese: any = {
+    janeiro: '1',
+    fevereiro: '2',
+    março: '3',
+    abril: '4',
+    maio: '5',
+    junho: '6',
+    julho: '7',
+    agosto: '8',
+    setembro: '9',
+    outubro: '10',
+    novembro: '11',
+    dezembro: '12'
+    };
+    export const monthNamePortuguese: any = {
+    1 : 'janeiro',
+    2 : 'fevereiro',
+    3 : 'março',
+    4 : 'abril',
+    5 : 'maio',
+    6 : 'junho',
+    7 : 'julho',
+    8 : ' agosto',
+    9 : ' setembro',
+    10 : 'outubro',
+    11 : 'novembro',
+    12 : 'dezembro'
+    };
+    export const shortmonthNumPortuguese: any = {
+    jan: '1',
+    fev: '2',
+    mar: '3',
+    abr: '4',
+    mai: '5',
+    jun: '6',
+    jul: '7',
+    ago: '8',
+    set: '9',
+    out: '10',
+    nov: '11',
+    dez: '12'
+    };
+    export const monthNumPortugueseBrazil: any = {
+        janeiro: '1',
+        fevereiro: '2',
+        março: '3',
+        abril: '4',
+        maio: '5',
+        junho: '6',
+        julho: '7',
+        agosto: '8',
+        setembro: '9',
+        outubro: '10',
+        novembro: '11',
+        dezembro: '12'
+    };
+    export const monthNamePortugueseBrazil: any = {
+        1: 'janeiro',
+        2: 'fevereiro',
+        3: 'março',
+        4: 'abril',
+        5: 'maio',
+        6: 'junho',
+        7: 'julho',
+        8: 'agosto',
+        9: 'setembro',
+        10: 'outubro',
+        11: 'novembro',
+        12: 'dezembro'
+    };
+    export const shortmonthNumPortugueseBrazil: any = {
+        jan: '1',
+        fev: '2',
+        mar: '3',
+        abr: '4',
+        mai: '5',
+        jun: '6',
+        jul: '7',
+        ago: '8',
+        set: '9',
+        out: '10',
+        nov: '11',
+        dez: '12'
+    }
+}
